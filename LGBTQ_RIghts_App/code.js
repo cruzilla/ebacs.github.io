fc80cfe7ce83d09f654a352382e1f8706a3365cf
ebacs <159556611+ebacs@users.noreply.github.com>
2024-03-14 10:34:18 -0400

    Add files via upload

---
+++ LGBTQ_RIghts_App/code.js
@@ -0,0 +1,88 @@
+setProperty("enterbutton", "border-color", "black");
+onEvent("enterbutton", "click", function( ) {
+  setScreen("welcomescreen");
+});
+setProperty("helpbutton", "border-color", "black");
+onEvent("helpbutton", "click", function( ) {
+  setScreen("Helpscreen");
+});
+setProperty("factsbutton", "border-color", "black");
+onEvent("factsbutton", "click", function( ) {
+  setScreen("Factsscreen");
+});
+setProperty("welcomescreenbackbutton", "border-color", "black");
+onEvent("welcomescreenbackbutton", "click", function( ) {
+  setScreen("Signupscreen");
+});
+setProperty("helpscreenbackbutton", "border-color", "black");
+onEvent("helpscreenbackbutton", "click", function( ) {
+  setScreen("welcomescreen");
+});
+setProperty("factsbackbutton", "border-color", "black");
+onEvent("factsbackbutton", "click", function( ) {
+  setScreen("welcomescreen");
+});
+setProperty("selfconfidencebutton", "border-color", "black");
+onEvent("selfconfidencebutton", "click", function( ) {
+  setScreen("selfconfidencescreen");
+});
+setProperty("selfconfidencescreenbackbutton", "border-color", "black");
+onEvent("selfconfidencescreenbackbutton", "click", function( ) {
+  setScreen("Helpscreen");
+});
+setPosition("dealingwithhatebutton", "border-color", "black");
+onEvent("dealingwithhatebutton", "click", function( ) {
+  setScreen("dealingwithhatescreen");
+});
+setProperty("dealingwithhatebackbutton", "border-color", "black");
+onEvent("dealingwithhatebackbutton", "click", function( ) {
+  setScreen("Helpscreen");
+});
+setProperty("dealingwithinsecuritesbutton", "border-color", "black");
+onEvent("dealingwithinsecuritesbutton", "click", function( ) {
+  setScreen("dealingwithinsecuritiesscreen");
+});
+setProperty("insecuritiesbackbutton", "border-color", "black");
+onEvent("insecuritiesbackbutton", "click", function( ) {
+  setScreen("Helpscreen");
+});
+setProperty("beingtruetoyourselfbutton", "border-color", "black");
+onEvent("beingtruetoyourselfbutton", "click", function( ) {
+  setScreen("beingtruetoyourselfscreen");
+});
+setProperty("truetoyourselfbackbutton", "border-color", "black");
+onEvent("truetoyourselfbackbutton", "click", function( ) {
+  setScreen("Helpscreen");
+});
+setProperty("lgbtqmeaningbutton", "border-color", "black");
+onEvent("lgbtqmeaningbutton", "click", function( ) {
+  setScreen("LGBTQ+meaningscreen");
+});
+setProperty("LGBTQ+meaningbackbutton", "border-color", "black");
+onEvent("LGBTQ+meaningbackbutton", "click", function( ) {
+  setScreen("Factsscreen");
+});
+setProperty("againstmembersbutton", "border-color", "black");
+onEvent("againstmembersbutton", "click", function( ) {
+  setScreen("discriminationagainstLGBTQ+membersscreen");
+});
+setProperty("discriminationbackbutton", "border-color", "black");
+onEvent("discriminationbackbutton", "click", function( ) {
+  setScreen("Factsscreen");
+});
+setProperty("lgbtqrightsbutton", "border-color", "black");
+onEvent("lgbtqrightsbutton", "click", function( ) {
+  setScreen("lgbtqrightsscreen");
+});
+setProperty("lgbtqrightsbackbutton", "border-color", "black");
+onEvent("lgbtqrightsbackbutton", "click", function( ) {
+  setScreen("Factsscreen");
+});
+setProperty("whydoesdiscriminationhappenbutton", "border-color", "black");
+onEvent("whydoesdiscriminationhappenbutton", "click", function( ) {
+  setScreen("whydoesdiscriminationhappenscreen");
+});
+setProperty("backwhydiscriminationhappensbutton", "border-color", "black");
+onEvent("backwhydiscriminationhappensbutton", "click", function( ) {
+  setScreen("Factsscreen");
+});
